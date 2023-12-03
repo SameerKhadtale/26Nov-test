34c5ff700531958485defc428bfe6f1e39c6ad99
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-12-03 17:02:07 +0530

    updates

--- test/src/App.js
+++ test/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import  Reaact , {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const InvoiceCalculator = () => {
+const [quantity , setQuantity] = useState(0);
+const [price , setPrice] = useState(0);
+const [total , setTotal] = useState(0);
+
+const handleQualityChange = (event) => {
+const  newQuantity = parseInt(event.target.value, 10)  || 0;
+setQuantity(newQuantity);
+setTotal(setTotal, price);
+};
+
+const handlePriceChange = (event) =>{
+  const newPrice = parsFloat(event.target.value)  || 0;
+setPrice(newPrice);
+updateTotal(quantity, newPrice);
+};
+
+const updateTotal = (newQuantity, newPrice) =>{
+const newTotal = (newQuantity * newPrice)
+setTotal(newTotal);
+};
+return(
+  <div>
+    <h1>Invoice Calculator</h1>
+    <lable>Quantity</lable>
+    <input type='number' value={price} onChange={handleQualityChange}/>
+    <lable>Cost Price</lable>
+    <input type='number' value={price} onChange={handlePriceChange}/>
+    <lable>Margin %</lable>
+    <input type='number' value={Number} onChange={updateTotal}/>
+    <lable>Discount %</lable>
+    <input type='number' value={Number} onChange={updateTotal}/>
+    <lable>Tax %</lable>
+    <input type='number' value={Number} onChange={updateTotal}/>
+    <button>Submit</button>
+  </div>
+)
 }
+<table>
+  <body>
+    
+  </body>
+</table>
+
+
+// import React from 'react';
+// function App() {
+//   
 
 export default App;
