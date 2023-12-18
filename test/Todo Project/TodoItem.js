80130f703a8b5d883ae92440292893ac664af79e
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-12-18 18:26:47 +0530

    updates

---
+++ test/Todo Project/TodoItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TodoItem = ({todo, toggletodo, deletetodo}) =>{
+    return (
+        <li style={{ textDecoration : todo.completed ? 'line-through' : 'none'}} 
+        />
+        if(newTodo == )
+    )
+}
---
+++ test/Todo Project/todo.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import TodoItem from './TodoItem.js';
+
+const TodoList = () =>{
+    const[todos,setTodos] = useState([]);
+    const[newTodos, setNewTodos] = useState('');
+}
+const addTodo = () =>{
+    if (newTodo.trim() !== ''){
+        const 
+    }
+}
