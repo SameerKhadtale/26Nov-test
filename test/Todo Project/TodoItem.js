eade4084b339776596fce7e5a1b1ae6fd1665119
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2024-03-17 20:01:02 +0530

    updates

--- test/Todo Project/TodoItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TodoItem = ({todo, toggletodo, deletetodo}) =>{
-    return (
-        <li style={{ textDecoration : todo.completed ? 'line-through' : 'none'}} 
-        />
-        if(newTodo == )
-    )
-}
